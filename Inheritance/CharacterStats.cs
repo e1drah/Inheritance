ab37d79225e9017966ccb8f8e9af3f7b86235fd9
e1drah <113189459+e1drah@users.noreply.github.com>
2023-01-16 16:12:22 -0400

    Initial commit

---
+++ Inheritance/CharacterStats.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal abstract class CharacterStats
+    {
+        public string name;
+
+        public int strenth;
+        public int lives = 1;
+        public bool dead = false;
+
+        int maxHealth;
+        int health;
+        public void SetHealth(int hp)
+        {
+            maxHealth = hp;
+            health = hp;
+        }
+        public void TakeDamage(int damage, string attacker)
+        {
+            Console.WriteLine(attacker + " deals " + damage + " points of damage to " + name);
+            health -= damage;
+            if (health < 0)
+            {
+                Console.WriteLine(name + "falls to the ground...");
+                if (lives == 0)
+                {
+                    Console.WriteLine("...and stays there...");
+                    health = 0;
+                    dead = true;
+                }
+                else if (lives > 0)
+                {
+                    Console.WriteLine(".. and gets back up!!!");
+                    lives -= 1;
+                    health = maxHealth;
+                }
+            }
+        }
+        public void Heal(int hp)
+        {
+            Console.WriteLine(name + " heals " + hp + " points of damage");
+            health += hp;
+            if (health > maxHealth)
+            {
+                health = maxHealth;
+            }
+        }
+        public int DealDamage()
+        {
+            Random rng = new Random();
+            int damage = rng.Next(1, strenth);
+
+            return(damage);
+        }
+        
+        public void ShowStats()
+        {
+            Console.WriteLine(name + " health: " + health + "/" + maxHealth + " strenth: " + strenth + " Lives: " + lives);
+        }
+
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Player player = new Player();
+            player.SetHealth(rnd.Next(1,100));
+            player.strenth = rnd.Next(1, 25);
+            player.name = "Jerry";
+            player.lives = rnd.Next(1, 10);
+
+            Enemy enemy = new Enemy();
+            enemy.SetHealth(rnd.Next(1, 100));
+            enemy.strenth = rnd.Next(1, 25);
+            enemy.name = "Goblin";
+            enemy.lives = rnd.Next(1, 10); ;
+            player.ShowStats();
+            enemy.ShowStats();
+
+            while (enemy.dead == false && player.dead == false)
+            {
+                Console.WriteLine("----------------------------------------------");
+                enemy.TakeDamage(player.DealDamage(), player.name);
+                player.TakeDamage(enemy.DealDamage(), enemy.name);
+                Console.WriteLine("");
+                player.ShowStats();
+                enemy.ShowStats();
+                Console.WriteLine("----------------------------------------------");
+            }
+        
+            if (player.dead == true)
+            {
+                Console.WriteLine("Game Over");
+                Console.WriteLine("Press any button to exit");
+                Console.ReadKey(true);
+            }
+            if (enemy.dead == true)
+            {
+                Console.WriteLine("You Win");
+                Console.WriteLine("Press any button to exit");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
